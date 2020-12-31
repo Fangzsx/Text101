4430fbfaed60016c0e86ec87d5bb14fb74b62317
Fangszx <jygrzn@gmail.com>
2020-12-31 15:24:07 +0800

    Added a script to change the text programmatically.

---
+++ Text101/Assets/AdventureGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("Gunfires. Explosions. Sounds of rotary engines. Seeing Ben's falling comrades one by one realized that this is already defeat. It never struck to him that this day will come. The day that he feared the most.\nSuddenly, Ben heard a very large noise and all things went bright. \n\n\n\nPress Enter to Continue.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
