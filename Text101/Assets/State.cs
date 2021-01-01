36dedd173a40d768e8e80b09dec7faf31a7957d4
Fangszx <jygrzn@gmail.com>
2021-01-01 14:06:31 +0800

    Use scriptable objects to modify text in canvas

---
+++ Text101/Assets/State.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject {
+    
+    [TextArea(14,10)][SerializeField] string storyText;
+
+    public string GetStateStory(){
+        
+        return storyText;
+
+    }
+
+
+}
+
+  
+
