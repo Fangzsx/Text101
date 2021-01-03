aef7993c452a5f844b9122b38ffc8bbaff1c07fe
Fangszx <jygrzn@gmail.com>
2021-01-03 12:29:19 +0800

    Manage states

--- Text101/Assets/AdventureGame.cs
+++ Text101/Assets/AdventureGame.cs
@@ -6,6 +6,7 @@ public class AdventureGame : MonoBehaviour
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
+    int[] oddNumbers = { 1, 3, 5, 7, 9};
     State state;
 
     // Start is called before the first frame update
@@ -13,11 +14,31 @@ public class AdventureGame : MonoBehaviour
     {
         state = startingState;
         textComponent.text = state.GetStateStory();
+        Debug.Log(oddNumbers[3]);
 
     }
     // Update is called once per frame
     void Update()
     {
-        
+        ManageState();
     }
+
+    private void ManageState()
+    {
+        var nextStates = state.GetNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = nextStates[0];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha2)){
+            state = nextStates[1];
+        }
+        else if(Input.GetKeyDown(KeyCode.Alpha3)){
+            state = nextStates[2];
+        }
+
+        textComponent.text = state.GetStateStory();
+    }
+
+
 }
